3bfeb68974050b5adfd6c00456bedf99a4ad65e3
Gabby Jones <jones.gm90@gmail.com>
2019-12-16 22:56:53 -0500

    adding css styling

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,10 +1,13 @@
-var title = "the+notebook";
-var queryURL = ;
+var city = $("#searchBar").val();
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={3bf7feb8cded7176958e566058a498c2}";
 
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function(response) {
-  console.log(response);
-  console.log(response.Runtime);
-});
+})
+  // After data comes back from the request
+  .then(function(response) {
+    console.log(queryURL);
+
+    console.log(response);
+  });
