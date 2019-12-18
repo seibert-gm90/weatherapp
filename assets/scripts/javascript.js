8dd2f333cc10f8fac89140f6db451b33fb2b871f
Gabby Jones <jones.gm90@gmail.com>
2019-12-18 18:25:56 -0500

    adding functions to make AJAX call on click and populate into appropriate place on page

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,5 +1,12 @@
-var city = $("#searchBar").val();
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={3bf7feb8cded7176958e566058a498c2}";
+
+//var hardCodeCity= "Las Vegas"
+//var id="524901"
+//var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=3bf7feb8cded7176958e566058a498c2"
+
+$("#searchButton").click(function(){
+var city = $("#cityName").val();
+console.log(city)
+var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=3bf7feb8cded7176958e566058a498c2"
 
 $.ajax({
   url: queryURL,
@@ -7,7 +14,23 @@ $.ajax({
 })
   // After data comes back from the request
   .then(function(response) {
-    console.log(queryURL);
-
+    
     console.log(response);
+    var windSpeed=response.wind.speed 
+    
+    var humidity=response.main.humidity
+
+    var kelvinTemp=response.main.temp
+    
+    var tempF=kelvinTemp * 1.8 - 459.67
+    
+
+   var displayTemp= $("#temp").append(tempF)
+   var displayHum= $("#humidity").append(humidity)
+   var displayWS= $("#windSpeed").append(windSpeed)
+  
+    
   });
+  
+}); 
+ 
